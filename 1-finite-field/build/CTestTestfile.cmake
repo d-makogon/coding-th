a9cedfb70265effcd7aa7c391652debf8d32218f
Dmitry Makogon <d.makogon@g.nsu.ru>
2022-03-28 03:34:11 +0700

    Add gitignore

--- 1-finite-field/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmakogon/projects/coding-th/1-finite-field
-# Build directory: /home/dmakogon/projects/coding-th/1-finite-field/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("app")
